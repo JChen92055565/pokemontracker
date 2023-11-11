61c655aa4382ecc47f62c6fb49f640f8bc6d9046
Jasper Chen <japeeeeee@gmail.com>
2023-11-10 20:39:50 -0800

    Initial framework: folder structure

---
+++ src/components/Main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Main = () => 
+{
+    return 
+    (
+        <div>
+        <h1>Pokemon Set Tracker</h1>
+        {/* Additional content can be added here */}
+      </div>
+    )
+}
